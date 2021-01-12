6f98a52e88171c52225b5a29bbdf5264300fc498
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-12 13:20:34 -0500

    feat(add Product and User models)

---
+++ server/db/models/Product.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+const Product = db.define('Product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    unique: true
+  },
+  totalDownloads: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  likes: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  tags: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+    defaultValue: []
+  }
+})
+
+module.exports = Product
