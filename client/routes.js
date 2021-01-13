2be1a1ee4c69c080ec587555190dbe864db4e39e
nedbrennan <nedbrnnn@gmail.com>
2021-01-13 11:10:06 -0500

    fixes routing issue

--- client/routes.js
+++ client/routes.js
@@ -22,6 +22,7 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
+        <Route path="/" component={AllProducts} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/products" component={AllProducts} />
