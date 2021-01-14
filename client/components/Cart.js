a8ceba6c8c58b5b80fa23cd224a890cb25a8f114
Morgan <morgan.hu738@gmail.com>
2021-01-14 10:48:26 -0500

    fix(add package-lock, package.json, and db.js to gitignore)

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -13,7 +13,7 @@ export class Cart extends React.Component {
     return (
       <div>
         <h1> Cart </h1>
-        <div>
+        <div id="root">
           {cart.map(product => (
             <div key={product.id}>
               <h2> Product Name: {product.name} </h2>
