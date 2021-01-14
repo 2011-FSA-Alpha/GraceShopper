598053f9a8f616f6163ea5e22676e405b6621059
Azriel <goldmanazriel@yahoo.com>
2021-01-14 14:32:14 -0500

    Added an AddProduct form, in process of connecting to store, and completed SingleProduct React tests

---
+++ client/components/singleProduct.spec.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {expect} from 'chai'
+import {configure, shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import {SingleProduct} from './SingleProduct'
+
+configure({adapter: new Adapter(), disableLifecycleMethods: true})
+
+describe('SingleProduct component', () => {
+  const product = {
+    title: 'Windows',
+    description: 'A computer wallpaper',
+    price: 40,
+    imageUrl: 'image',
+    totalDownloads: 18,
+    likes: 94,
+    tags: ['hot', 'awesome']
+  }
+
+  it('renders out a div', () => {
+    const wrapper = shallow(<SingleProduct product={product} />)
+    expect(wrapper.find('div')).to.have.length(7)
+  })
+
+  it('renders out an h3 tag', () => {
+    const wrapper = shallow(<SingleProduct product={product} />)
+    expect(wrapper.find('h3').text()).to.include('Product Info:')
+  })
+
+  it('renders a product title, description, and price', () => {
+    const wrapper = shallow(<SingleProduct product={product} />)
+    expect(wrapper.text()).to.include('Product Title: Windows')
+    expect(wrapper.text()).to.include(
+      'Product Description: A computer wallpaper'
+    )
+    expect(wrapper.text()).to.include('Product Price: $', 40)
+  })
+
+  it('renders a product total downloads, likes and tags', () => {
+    const wrapper = shallow(<SingleProduct product={product} />)
+    expect(wrapper.text()).to.include('Total downloads:', 18)
+    expect(wrapper.text()).to.include('Total # of Likes:', 94)
+    expect(wrapper.text()).to.include('Tags: hotawesome')
+  })
+
+  it('renders an image', () => {
+    const wrapper = shallow(<SingleProduct product={product} />)
+    expect(wrapper.find('img')).to.have.length(1)
+  })
+})
--- client/store/singleProduct.js
+++ client/store/singleProduct.js
@@ -1,15 +1,15 @@
 import axios from 'axios'
 
 //initial state
-const initialState = {
-  product: {}
-}
+const initialState = {}
 
 //action type
 const SET_PRODUCT = 'SET_PRODUCT'
+const ADD_PRODUCT = 'ADD_PRODUCT'
 
 //action creator
 export const setProduct = product => ({type: SET_PRODUCT, product})
+export const addProduct = product => ({type: ADD_PRODUCT, product})
 
 //thunk creator
 export const fetchProduct = id => {
@@ -25,10 +25,25 @@ export const fetchProduct = id => {
   }
 }
 
+export const addProductThunk = (id, product) => {
+  return async dispatch => {
+    try {
+      const res = await axios.post(`/api/products/${id}`, product)
+      const newProduct = res.data
+      dispatch(addProduct(newProduct))
+    } catch (error) {
+      console.log('Unable to add a new product', error)
+      console.error(error)
+    }
+  }
+}
+
 //reducer
 export default function productReducer(state = initialState, action) {
   switch (action.type) {
     case SET_PRODUCT:
+      return action.product
+    case ADD_PRODUCT:
       return action.product
     default:
       return state
