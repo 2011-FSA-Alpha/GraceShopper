e5f70171f14310d6a446463b065914be947c546e
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-14 17:25:28 -0500

    feat(add OrderConfirmed component and tests)

--- client/components/OrderConfirmed.js
+++ client/components/OrderConfirmed.js
@@ -4,8 +4,8 @@ const OrderConfirmed = props => {
   return (
     <div>
       <h1>🥳 Order Confirmed 🥳</h1>
-      <h3>Your order number is: {props.orderId}</h3>
-      <h3>Your total is: {props.total}</h3>
+      <h3 className="order-id">Your order number is: #{props.orderId}</h3>
+      <h3 className="order-total">Your total is: {props.total}</h3>
     </div>
   )
 }
