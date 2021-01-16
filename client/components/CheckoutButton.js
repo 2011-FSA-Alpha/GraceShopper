45925c7143a8b5670cc1966bdf27c9fc2a3fac31
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-15 21:45:44 -0500

    fix(disable Checkout when cart is empty)

--- client/components/CheckoutButton.js
+++ client/components/CheckoutButton.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {canTreatArrayAsAnd} from 'sequelize/types/lib/utils'
 
 // This is a dummy button that will eventually lead to a Checkout page to prompt the user for payment details
 
 const CheckoutButton = props => {
+  const {cart} = props
   return (
     <Link to="/order-confirmed">
-      <button>Checkout</button>
+      <button disabled={cart.length ? 'false' : 'true'}>Checkout</button>
     </Link>
   )
 }
