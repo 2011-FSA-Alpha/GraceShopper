8354c3426924fe8111144a2a66b73666d2505d2c
nedbrennan <nedbrnnn@gmail.com>
2021-01-16 13:37:02 -0500

    fix(gets rid of bugs when loading an empty cart)

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -3,7 +3,18 @@ import {connect} from 'react-redux'
 import {showCart, deleteCartItem, incrementQuantity} from '../store/cart'
 
 export class Cart extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      loading: true
+    }
+  }
+
   componentDidMount() {
+    setTimeout(() => {
+      this.setState({loading: false})
+    }, 200)
     if (this.props.user.id) {
       this.props.showCart(this.props.user.id)
     }
@@ -20,55 +31,59 @@ export class Cart extends React.Component {
     return (
       <div>
         <h1> Cart </h1>
-        <div id="root">
-          {cart.products ? (
-            cart.products.map(product => (
-              <div key={product.id}>
-                <h1> Product Name: {product.title} </h1>
-                <h3> Price: {product.price} </h3>
-                <h3> Quantity: {product.orderproducts.quantity} </h3>
-                <button
-                  type="button"
-                  onClick={() =>
-                    this.props.incrementQuantity(this.props.user.id, {
-                      productId: product.id,
-                      orderId: this.props.cart.id,
-                      quantity: 1
-                    })
-                  }
-                >
-                  +
-                </button>
-                <button
-                  type="button"
-                  onClick={() =>
-                    this.props.incrementQuantity(this.props.user.id, {
-                      productId: product.id,
-                      orderId: this.props.cart.id,
-                      quantity: -1
-                    })
-                  }
-                >
-                  -
-                </button>
-                <h3> Picture of product </h3>
-                <button
-                  type="button"
-                  onClick={() =>
-                    this.props.deleteCartItem(this.props.user.id, {
-                      productId: product.id,
-                      orderId: this.props.cart.id
-                    })
-                  }
-                >
-                  Delete
-                </button>
-              </div>
-            ))
-          ) : (
-            <h2>Your cart is empty</h2>
-          )}
-        </div>
+        {this.state.loading ? (
+          <h2>Loading cart...</h2>
+        ) : (
+          <div id="root">
+            {cart.products.length > 0 ? (
+              cart.products.map(product => (
+                <div key={product.id}>
+                  <h1> Product Name: {product.title} </h1>
+                  <h3> Price: {product.price} </h3>
+                  <h3> Quantity: {product.orderproducts.quantity} </h3>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      this.props.incrementQuantity(this.props.user.id, {
+                        productId: product.id,
+                        orderId: this.props.cart.id,
+                        quantity: 1
+                      })
+                    }
+                  >
+                    +
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      this.props.incrementQuantity(this.props.user.id, {
+                        productId: product.id,
+                        orderId: this.props.cart.id,
+                        quantity: -1
+                      })
+                    }
+                  >
+                    -
+                  </button>
+                  <h3> Picture of product </h3>
+                  <button
+                    type="button"
+                    onClick={() =>
+                      this.props.deleteCartItem(this.props.user.id, {
+                        productId: product.id,
+                        orderId: this.props.cart.id
+                      })
+                    }
+                  >
+                    Delete
+                  </button>
+                </div>
+              ))
+            ) : (
+              <h2>Your cart is empty</h2>
+            )}
+          </div>
+        )}
       </div>
     )
   }
