ec080583162ca879713f382679b279d8aad267ca
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-17 18:28:07 -0500

    fix(update  fetchProduct path to clear error)

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {getProducts} from '../store/products'
 import {Link} from 'react-router-dom'
 import {addItemToCart, showCart} from '../store/cart'
-import {SingleProduct, fetchProduct} from './SingleProduct'
+import {fetchProduct} from '../store/singleProduct'
 
 export class AllProducts extends React.Component {
   constructor(props) {
