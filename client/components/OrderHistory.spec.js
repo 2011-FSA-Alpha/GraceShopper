6bf6da65276847b6a5cf053426a68237819f642c
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-18 14:23:16 -0500

    test(order history is dynamically generated)

--- client/components/OrderHistory.spec.js
+++ client/components/OrderHistory.spec.js
@@ -5,83 +5,69 @@ import Adapter from 'enzyme-adapter-react-16'
 import {OrderHistory} from './OrderHistory'
 
 const adapter = new Adapter()
-enzyme.configure({adapter})
+enzyme.configure({adapter, disableLifecycleMethods: true})
 
 describe('OrderHistory', () => {
-  beforeEach(() => {
-    let history
-    let wrapper
-    let user
-
-    user = [
-      {
-        adminStatus: false,
-        createdAt: '2021-01-18T02:48:00.671Z',
-        email: 'ricky.a.rhodes@gmail.com',
-        googleId: '101700391713633740360',
-        id: 6,
-        name: 'Ricky Rhodes'
-      }
-    ]
-
-    history = [
-      {
-        id: 1,
-        products: [
-          {
-            id: 1,
-            title: 'Dog Pic',
-            description: 'Picture of dog',
-            price: 100,
-            imageUrl: 'https://picsum.photos/200/300'
-          },
-          {
-            id: 2,
-            title: 'Dog Pic',
-            description: 'Picture of dog',
-            price: 100,
-            imageUrl: 'https://picsum.photos/200/300'
-          },
-          {
-            id: 3,
-            title: 'Dog Pic',
-            description: 'Picture of dog',
-            price: 100,
-            imageUrl: 'https://picsum.photos/200/300'
-          }
-        ]
-      },
-      {
-        id: 2,
-        products: [
-          {
-            id: 1,
-            title: 'Dog Pic',
-            description: 'Picture of dog',
-            price: 100,
-            imageUrl: 'https://picsum.photos/200/300'
-          },
-          {
-            id: 2,
-            title: 'Dog Pic',
-            description: 'Picture of dog',
-            price: 100,
-            imageUrl: 'https://picsum.photos/200/300'
-          },
-          {
-            id: 3,
-            title: 'Dog Pic',
-            description: 'Picture of dog',
-            price: 100,
-            imageUrl: 'https://picsum.photos/200/300'
-          }
-        ]
-      }
-    ]
-    wrapper = shallow(<OrderHistory orderHistory={history} user={user} />)
-  })
-
   it('dynamically renders all of a users past orders', () => {
-    console.log(history.orderHistory)
+    const props = {
+      orderHistory: [
+        {
+          id: 1,
+          products: [
+            {
+              id: 1,
+              title: 'Dog Pic',
+              description: 'Picture of dog',
+              price: 100,
+              imageUrl: 'https://picsum.photos/200/300'
+            },
+            {
+              id: 2,
+              title: 'Dog Pic',
+              description: 'Picture of dog',
+              price: 100,
+              imageUrl: 'https://picsum.photos/200/300'
+            },
+            {
+              id: 3,
+              title: 'Dog Pic',
+              description: 'Picture of dog',
+              price: 100,
+              imageUrl: 'https://picsum.photos/200/300'
+            }
+          ]
+        },
+        {
+          id: 2,
+          products: [
+            {
+              id: 1,
+              title: 'Dog Pic',
+              description: 'Picture of dog',
+              price: 100,
+              imageUrl: 'https://picsum.photos/200/300'
+            },
+            {
+              id: 2,
+              title: 'Dog Pic',
+              description: 'Picture of dog',
+              price: 100,
+              imageUrl: 'https://picsum.photos/200/300'
+            },
+            {
+              id: 3,
+              title: 'Dog Pic',
+              description: 'Picture of dog',
+              price: 100,
+              imageUrl: 'https://picsum.photos/200/300'
+            }
+          ]
+        }
+      ],
+      fetchOrderHistory: () => console.log('helloWorld')
+    }
+
+    let wrapper = shallow(<OrderHistory {...props} />)
+    expect(wrapper.find('div.order-history')).to.have.lengthOf(2)
   })
 })
