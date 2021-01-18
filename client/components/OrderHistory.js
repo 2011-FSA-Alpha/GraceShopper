cd3cf6ae6efda17ea3a240429b038ca3945ab4b1
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-17 21:20:03 -0500

    chore(add order history to seed file)

---
+++ client/components/OrderHistory.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OrderHistory = props => {
+  return (
+    <div>
+      {/* Map through the order history */}
+      <h1>Map the history</h1>
+    </div>
+  )
+}
+
+export default OrderHistory
