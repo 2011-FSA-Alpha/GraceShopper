2dc8a3d5b060375cd614ef5fcf7b935c14063d5e
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-20 12:12:33 -0500

    feat(add stripe package and api router)

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,7 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/order', require('./order'))
 router.use('/products', require('./products'))
+router.use('/payment', require('./payment'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
