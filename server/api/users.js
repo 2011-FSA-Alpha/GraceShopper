32de1d13e48cd7faec7851ed2dbde66bdc1ac49e
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-19 20:54:36 -0500

    chore(clean up)

--- server/api/users.js
+++ server/api/users.js
@@ -3,9 +3,9 @@ const {User} = require('../db/models')
 const adminOnly = require('../util/adminOnly')
 module.exports = router
 
-router.get('/', adminOnly, async (req, res, next) => {
+// GET /api/users
+router.get('/', async (req, res, next) => {
   try {
-    console.log('hello', req.query)
     const users = await User.findOrCreate({
       where: {
         name: req.query.name,
