cc937fc96f3e3b2ad9bba6d62b7b54440b00dd12
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-20 17:28:49 -0500

    feat(add tagline under logo)

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import AllProducts from './AllProducts'
 import Carousel from './Carousel'
+import {Typography, Box} from '@material-ui/core'
 
 class Home extends Component {
   render() {
@@ -21,7 +22,23 @@ class Home extends Component {
               fontSize: '100px'
             }}
           >
-            <img src="https://dl.dropboxusercontent.com/s/vi19joy6hboavgo/wallpaper_logo_web.png?dl=0" />
+            <img
+              src="https://dl.dropboxusercontent.com/s/vi19joy6hboavgo/wallpaper_logo_web.png?dl=0"
+              style={{height: '70%', width: '70%', paddingLeft: '1.5rem'}}
+            />
+            <Box display="flex" justifyContent="center">
+              <Typography
+                variant="h4"
+                style={{color: 'white', alignSelf: 'center'}}
+              >
+                Wallpaper
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" style={{color: 'white'}}>
+                A marketplace for the best digital wallpapers on the internet.
+              </Typography>
+            </Box>
           </div>
           <Carousel />
         </div>
