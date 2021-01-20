c811ccc609da16e67560bd57e6246562dc1ac81e
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-20 12:19:35 -0500

    test(manaully via Postman that Stripe Payment Intents are integrated)

--- server/api/payment.js
+++ server/api/payment.js
@@ -1,3 +1,25 @@
 const router = require('express').Router()
+const Stripe = require('stripe')
+// ADD ENV VAR
+const stripe = Stripe(
+  'sk_test_51IBgClBOhqQyDiVzyJbg010oMYzMS3uabbC5ngPIULdfl5IUTvp0y5tmoIIRIcrNnjKMmbAQuhM9njNxOOhkRTs700oEcH0V4d'
+)
+
+// Mounted on /api/payment
+
+router.post('/add', async (req, res, next) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: 1000,
+      currency: 'usd',
+      payment_method_types: ['card'],
+      receipt_email: 'jenny.rosen@example.com'
+    })
+
+    res.json(paymentIntent)
+  } catch (err) {
+    console.error(err)
+  }
+})
 
 module.exports = router
