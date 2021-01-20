63dd309842ab4f298eb2bfae6b0f81c0f6868ad5
nedbrennan <nedbrnnn@gmail.com>
2021-01-20 14:32:38 -0500

    feat(styles user-home)

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import OrderHistory from './OrderHistory'
+import {Card, CardContent, Typography, Box, Paper} from '@material-ui/core'
 
 /**
  * COMPONENT
@@ -11,15 +12,49 @@ export const UserHome = props => {
   const firstName = name.split(' ')[0]
   //const lastName = name.split(' ')[1]
   return (
-    <div>
-      <h2>Welcome to your Wallpaper account, {firstName}!</h2>
-      <h3 style={{textDecoration: 'underline'}}> User Info </h3>
-      <h4> Name: {name}</h4>
-      <h4> Email: {email}</h4>
-      <h4> Admin Status: {adminStatus ? 'Yes' : 'No'}</h4>
-      <h3>Past Orders:</h3>
+    <React.Fragment>
+      <Box position="relative" paddingTop="40px" paddingLeft="40px">
+        <Typography variant="h4" style={{color: 'lightgray'}}>
+          Welcome to your Wallpaper account, {firstName}!
+        </Typography>
+      </Box>
+      <Box
+        position="relative"
+        paddingLeft="40px"
+        paddingTop="40px"
+        width="60vw"
+        alignSelf="center"
+      >
+        <Paper style={{backgroundColor: '#333333'}}>
+          <CardContent>
+            <Typography variant="h5" style={{color: 'lightgray'}}>
+              {' '}
+              User Info{' '}
+            </Typography>
+            <Typography variant="h5" style={{color: 'lightgray'}}>
+              {' '}
+              Name: {name}
+            </Typography>
+            <Typography variant="h5" style={{color: 'lightgray'}}>
+              {' '}
+              Email: {email}
+            </Typography>
+            {adminStatus ? (
+              <Typography variant="h5" style={{color: 'lightgray'}}>
+                {' '}
+                Admin Status: You are a Wallpaper administrator
+              </Typography>
+            ) : null}
+          </CardContent>
+        </Paper>
+      </Box>
+      <Box position="relative" paddingTop="40px" paddingLeft="40px">
+        <Typography variant="h5" style={{color: 'lightgray'}}>
+          Order History:
+        </Typography>
+      </Box>{' '}
       <OrderHistory />
-    </div>
+    </React.Fragment>
   )
 }
 
