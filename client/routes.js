11cf7a4feb0b5e647e42e13f13288401a66ee22f
Ricky Rhodes <36062933+h0plyn@users.noreply.github.com>
2021-01-20 12:04:52 -0500

    Update routes.js

--- client/routes.js
+++ client/routes.js
@@ -44,8 +44,8 @@ class Routes extends Component {
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
         <Route component={NotFound} />
+        <Route component={Login} />
       </Switch>
     )
   }
