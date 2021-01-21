5aff16bdcf8ab25c37046234ac2ce10ccc0d3431
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-20 22:21:48 -0500

    feat(add loading spinner)

--- client/components/Carousel.js
+++ client/components/Carousel.js
@@ -28,19 +28,19 @@ export default class Carousel extends React.Component {
         }}
       >
         <Slider {...settings}>
-          <div>
+          <div className="fade-in">
             <img
               src="https://dl.dropboxusercontent.com/s/pv51nbbwa8j3dct/201029_PRS_Idaho_197.jpg?dl=0
 "
             />
           </div>
-          <div>
+          <div className="fade-in">
             <img src="https://dl.dropboxusercontent.com/s/x4p3x6o07aur78j/RickyRhodes_TVC_Midtown_135.jpg?dl=0" />
           </div>
-          <div>
+          <div className="fade-in">
             <img src="https://dl.dropboxusercontent.com/s/n8xby9m7h4lkrp4/201029_PRS_Idaho_236.jpg?dl=0" />
           </div>
-          <div>
+          <div className="fade-in">
             <img src="https://dl.dropboxusercontent.com/s/jmnx0e0d48h3i0n/RickyRhodes_TVC_Midtown_180-Edit.jpg?dl=0" />
           </div>
         </Slider>
