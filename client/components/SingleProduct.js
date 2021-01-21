848eba3f24be69433d023d647e154912e9819350
Ricky Rhodes <ricky@rickyrhodes.com>
2021-01-21 00:23:53 -0500

    feat(styled SingleProduct)

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -3,6 +3,16 @@ import {connect} from 'react-redux'
 import {addItemToCart} from '../store/cart'
 import {fetchProduct} from '../store/singleProduct'
 import AdminEditProd from './AdminEditProd'
+import {
+  Typography,
+  Box,
+  Button,
+  CardContent,
+  Paper,
+  CardActions,
+  Card
+} from '@material-ui/core'
+import {spacing} from '@material-ui/system'
 
 export class SingleProduct extends React.Component {
   constructor(props) {
@@ -24,45 +34,105 @@ export class SingleProduct extends React.Component {
   componentDidUpdate(prevProps) {
     if (prevProps.products !== this.props.products) {
       this.props.showCart(this.props.user.id)
+      console.log(prevProps)
     }
   }
 
   render() {
     const {showEdit} = this.state
-
     let product = this.props.product
+
     return (
       <div className="fade-in">
         {product ? (
-          <div>
-            <h3>Product Info:</h3>
-
-            <button type="button" onClick={this.toggleEdit}>
-              {' '}
-              Edit Product{' '}
-            </button>
-
-            {showEdit ? <AdminEditProd product={product} /> : null}
-
-            <div>Product Title: {product.title}</div>
-            <div>Product Description: {product.description}</div>
-            <div>Product Price: ${product.price}</div>
-            <img style={{width: '50%', height: '50%'}} src={product.imageUrl} />
-            <div>Total downloads: {product.totalDownloads}</div>
-            <div>Total # of Likes: {product.likes}</div>
-            <div>Tags: {product.tags}</div>
-            <button
-              type="button"
-              onClick={() =>
-                this.props.addItemToCart(this.props.user.id, {
-                  productId: product.id,
-                  orderId: this.props.cart.id
-                })
-              }
+          <Box
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+          >
+            <Paper
+              elevation={10}
+              style={{
+                backgroundColor: '#333333',
+                marginTop: '2.5rem',
+                width: '60vw',
+                paddingBottom: '2rem',
+                marginBottom: '1rem'
+              }}
             >
-              Add to cart
-            </button>
-          </div>
+              <img
+                style={{width: '100%', height: '80%'}}
+                src={product.imageUrl}
+              />
+              <Box style={{margin: '2rem'}}>
+                <Typography variant="h3" className="dark">
+                  {product.title}
+                </Typography>
+                <Box>
+                  <Typography variant="h5" className="white">
+                    Artist's Description: {product.description}
+                  </Typography>
+                </Box>
+
+                <Typography variant="h5" className="white">
+                  Price: ${product.price / 100}
+                </Typography>
+                <Typography variant="h5" className="white">
+                  {product.totalDownloads} Downloads
+                </Typography>
+
+                <Typography variant="h5" className="white">
+                  Liked by {product.likes} others
+                </Typography>
+                <Typography variant="h5" className="white">
+                  Tags: {product.tags}
+                </Typography>
+              </Box>
+              <Box
+                style={{
+                  display: 'flex',
+                  justifyContent: 'center'
+                }}
+              >
+                <Button
+                  type="button"
+                  variant="contained"
+                  style={{
+                    background:
+                      'linear-gradient(45deg, #2D8DFF 30%, #9DCAFF 90%)'
+                  }}
+                  color="secondary"
+                  style={{
+                    background:
+                      'linear-gradient(45deg, #2D8DFF 30%, #9DCAFF 90%)'
+                  }}
+                  onClick={() =>
+                    this.props.addItemToCart(this.props.user.id, {
+                      productId: product.id,
+                      orderId: this.props.cart.id
+                    })
+                  }
+                >
+                  Add to cart
+                </Button>
+                <Button
+                  type="button"
+                  variant="contained"
+                  onClick={this.toggleEdit}
+                  style={{
+                    background:
+                      'linear-gradient(45deg, #2D8DFF 30%, #9DCAFF 90%)'
+                  }}
+                  color="secondary"
+                >
+                  Edit Product
+                </Button>
+                {showEdit ? <AdminEditProd product={product} /> : null}
+              </Box>
+            </Paper>
+          </Box>
         ) : (
           <h1>No product found</h1>
         )}
